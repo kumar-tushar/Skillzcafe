44a99a4f2e0d45d955f8f21a15345db0e023d0db
tushar-skillzcafe <tushar@skillzcafe.com>
2025-05-05 05:10:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+// Route: /welcome
+app.get("/welcome", (req, res) => {
+  res.send("Hello World");
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
