807b2866cc21156b64f7ab71818d862165053502
tushar-skillzcafe <tushar@skillzcafe.com>
2025-05-05 07:17:39 +0530

    Update restart_pm2.sh to add PM2 to PATH and modify restart process to stop and start the app by name instead of stopping and restarting all processes.

--- scripts/restart_pm2.sh
+++ scripts/restart_pm2.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Add PM2 to PATH
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v18.20.4/bin
+
 cd /var/www/Skillzcafe
-pm2 stop all
-pm2 restart all
+
+# Stop and start the app by name
+pm2 stop skillzcafeapi || true
+pm2 start index.js --name "skillzcafeapi"
+
