6b79428b35fe94e3b4810aa3cd938e37a74eaa67
tushar-skillzcafe <tushar@skillzcafe.com>
2025-05-05 20:13:46 +0530

    Refactor welcome route response message and clean up install_dependencies.sh script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
-
-
-echo "---------------------------------------"
-echo "Deployment Group: $DEPLOYMENT_GROUP_NAME"
-echo "Application Name: $APPLICATION_NAME"
-echo "---------------------------------------"
-
 cd /var/www/Skillzcafe
 sudo chown -R ubuntu:ubuntu /var/www/Skillzcafe
 
