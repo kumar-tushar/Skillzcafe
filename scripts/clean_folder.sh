69c6c343bab1be0c35404bcdbf7618ef0f966040
tushar-skillzcafe <tushar@skillzcafe.com>
2025-05-05 06:47:19 +0530

    Reduce timeout values in appspec.yml and update clean_folder.sh to preserve .git folder during cleanup

--- scripts/clean_folder.sh
+++ scripts/clean_folder.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd /var/www/Skillzcafe
 
-# Preserve .env, delete everything else
-shopt -s extglob
-rm -rf !(.env)
+# Preserve .env and .git folder, delete everything else
+find . -mindepth 1 ! -name '.env' ! -name '.git' ! -path './.git/*' -exec rm -rf {} +
